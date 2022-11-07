923a65e163ede5bd1dfc382914fa681f90be459b
Aaron Elkins <threcius@yahoo.com>
2022-11-07 13:25:38 +0800

    Improve header files

--- parser.h
+++ parser.h
@@ -1,5 +1,10 @@
+#include "tokenizer.h"
+
+#ifndef _PARSER_H
+#define _PARSER_H
 int parse_expression(Tokenizer *t);
 int parse_term(Tokenizer *t);
 int parse_expression2(Tokenizer *t);
 int parse_term2(Tokenizer *t);
 int parse_factor(Tokenizer *t);
+#endif // _PARSER_H
--- tokenizer.h
+++ tokenizer.h
@@ -26,4 +26,4 @@ char next_char(Tokenizer *tokenizer);
 char peek_char(Tokenizer *tokenizer);
 Token next_token(Tokenizer *tokenizer);
 Token peek_token(Tokenizer *tokenizer);
-#endif
+#endif // _TOKENIZER_H
